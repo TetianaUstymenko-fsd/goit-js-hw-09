927d781283c4f1325538e26d83c94400338d19bb
Tetiana Ustymenko <ustimenko.tetana7@gmail.com>
2025-05-08 20:52:50 +0300

    all

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,28 @@
 import { defineConfig } from 'vite';
+import FullReload from 'vite-plugin-full-reload';
+import injectHTML from 'vite-plugin-html-inject';
+import { globSync } from 'glob';
 
-export default defineConfig({
-  base: '/goit-js-hw-09/', // заміни на назву твого GitHub-репозиторію
-});
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/goit-js-hw-09/' : '/',
+  root: '.', // HTML-файли в корені
+  build: {
+    outDir: 'dist',
+    sourcemap: false, // можна увімкнути true для налагодження
+    emptyOutDir: true,
+    rollupOptions: {
+      input: globSync('./*.html'),
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./*.html']),
+  ],
+}));
